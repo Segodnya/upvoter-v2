b27bd31971bbff5428e36c8815df27e4c33f4f9e
Kirill Kurentsov <immortalsteel@gmail.com>
2023-09-04 13:46:20 +0300

    style: button and genre styles

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { AlbumCardProps } from "../types";
-import { Card, Button, ButtonGroup } from "react-bootstrap";
+import { Card, Button, ButtonGroup, Badge } from "react-bootstrap";
 import "./AlbumCard.css";
 
 export const AlbumCard: FC<AlbumCardProps> = ({
@@ -16,8 +16,12 @@ export const AlbumCard: FC<AlbumCardProps> = ({
         <Card.ImgOverlay className="card__overlay">
           <Card.Title>{album.title}</Card.Title>
           <Card.Text>{album.band}</Card.Text>
-          <Card.Text>Genres: {album.genre.join(", ")}</Card.Text>
-          <ButtonGroup className="me-2">
+          <Card.Text>
+            {album.genre.map((g) => (
+              <Badge pill bg="dark">{`#${g}`}</Badge>
+            ))}
+          </Card.Text>
+          <ButtonGroup size="sm">
             <Button variant="success" onClick={() => handleVote(index)}>
               Vote
             </Button>
